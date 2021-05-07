06804136aa4a9c82c3b075231834975b8cb4ca89
weberlu88 <43847259+weberlu88@users.noreply.github.com>
2021-05-07 17:25:32 +0800

    mob 自己寫v2

--- OOAD_Labs/src/mob_movie/Main.java
+++ OOAD_Labs/src/mob_movie/Main.java
@@ -38,18 +38,32 @@ class Customer {
 
 /** 一筆租片紀錄  電影:顧客的m:n關聯表 **/
 class Rental {
-	Customer customer;
-	Movie movie;
-	LocalDateTime datetime; // 消費時間
-	int point; // 單筆累積點數
-	int price; // 單筆消費金額
-	int duration; // 該片租借幾天
+	private Customer customer;
+	private Movie movie;
+	private LocalDateTime datetime; // 消費時間
+	private int point; // 單筆累積點數
+	private int price; // 單筆消費金額
+	private int duration; // 該片租借幾天
 	
 	public Rental(Customer customer, Movie movie, int duration) {
 		this.customer = customer;
 		this.movie = movie;
 		this.duration = duration;
 		datetime = LocalDateTime.now(); // 還需要point price
+		point = movie.getPoint();
+		price = movie.getPrice();
+	}
+	
+	// 計算租duration天，該片要多少錢
+	private int calculatePrice(Movie movie, int duration) {
+		int basic = 0, extra = 0;
+		if (basic <= movie.rentDurationLimit()) 
+			basic = movie.getNormalPricePerDay() * duration;
+		else {
+			basic = * movie.rentDurationLimit(); // 基本天數的價格
+			extra = // 額外天數的價格
+		}
+		return basic + extra;	
 	}
 }
 
